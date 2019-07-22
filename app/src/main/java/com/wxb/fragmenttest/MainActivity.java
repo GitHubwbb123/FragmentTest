94077b77d6848a91afc80f62d7a5c8e081982678
GitHubwbb123 <869881873@qq.com>
2019-07-22 21:54:44 +0800

    Initial commit

---
+++ app/src/main/java/com/wxb/fragmenttest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.wxb.fragmenttest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(this);
+        replaceFragmeen(new RightFragment());
+    }
+    private  void replaceFragmeen(Fragment fragment){
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.right_layout,fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.button:
+                replaceFragmeen(new ThirdFragment());
+                break;
+            default:
+                break;
+
+
+        }
+
+    }
+}
